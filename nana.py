9aede3d0d39b51e08040ca3fd300e2194c59ffb3
ghostayee <mayeeayee@outlook.com>
2026-08-22 13:28:55 +0300

    ui

--- nana.py
+++ nana.py
@@ -3,7 +3,20 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    print(f"{num_of_days} days are {num_of_days * calculator_to_units} {name_of_unit}")
+    if num_of_days > 0:
+        return (
+            f"{num_of_days} days are {num_of_days * calculator_to_units} {name_of_unit}"
+        )
+    else:
+        return "you have entered a negative value no calculation for you"
 
 
-days_to_units(56)
+my_var = days_to_units(56)
+print(my_var)
+
+user_input = input(
+    "Hey user Enter the number of days and i will convert it to hours: \n"
+)
+user_input_number = int(user_input)
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
