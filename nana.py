6f0d41812b3a7dc3f26f0c0b4b949de8196e6b21
ghostayee <mayeeayee@outlook.com>
2026-08-22 13:56:51 +0300

    ui

--- nana.py
+++ nana.py
@@ -3,25 +3,28 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
-        return (
-            f"{num_of_days} days are {num_of_days * calculator_to_units} {name_of_unit}"
-        )
-    elif num_of_days == 0:
-        return "you have entered zero enter a valid number"
-    else:
-        return "you have entered a negative value no calculation for you"
+    return f"{num_of_days} days are {num_of_days * calculator_to_units} {name_of_unit}"
 
 
-my_var = days_to_units(56)
-print(my_var)
+def validate_execute():
+    try:
+
+        if user_input.isdigit():
+            user_input_number = int(user_input)
+            if user_input_number > 0:
+                calculated_value = days_to_units(user_input_number)
+                print(calculated_value)
+            elif user_input_number == 0:
+                return "you have entered zero no calculation for you"
+            else:
+                print("you have entered a negative number no calculation for you ")
+        else:
+            return "invalid number do not ruin my programme"
+    except ValueError:
+        print("your number is not valid ")
+
 
 user_input = input(
     "Hey user Enter the number of days and i will convert it to hours: \n"
 )
-if user_input.isdigit():
-    user_input_number = int(user_input)
-    calculated_value = days_to_units(user_input_number)
-    print(calculated_value)
-else:
-    print("invalid number do not ruin my programme")
+validate_execute()
