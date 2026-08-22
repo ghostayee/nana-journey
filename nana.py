d246798f2e22355139398aa11f4fb75a0b8f621f
ghostayee <mayeeayee@outlook.com>
2026-08-22 12:54:05 +0300

    ui

---
+++ nana.py
@@ -0,0 +1,9 @@
+calculator_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calculator_to_units} {name_of_unit}")
+
+
+days_to_units(56)
